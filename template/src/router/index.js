c24add3bae70b33a82b2466eace8e59af0ca4250
limingying2580 <1536844110@qq.com>
2021-03-26 18:02:42 +0800

    vue2模板初始化(适用h5)

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -16,3 +16,72 @@ const router = new VueRouter({
 /* ----------------- custom ----------------- */
 
 export default router
+
+// router.beforeEach((to, from, next) => {
+//   let fullPath = to.fullPath
+//   if (fullPath == '/login') {
+//     util.cookies.deleteCookie('o-token')
+//     next()
+//   } else {
+//     const token = util.cookies.getJsonCookie('o-token')
+//     if (token) {
+//       next()
+//     } else {
+//       next({
+//         path: 'login'
+//       })
+//     }
+//   }
+// });
+
+
+/* ----------------- custom ----------------- */
+// import _navigator from "@/router/navigator";
+//
+// function _asNavigator(_nav) {
+//   let _map = new Map()
+//
+//   for (let item of _nav) {
+//     let _path = item.path;
+//     if (!_path) {
+//       _path = '/'
+//     }
+//
+//     let _route = _map.get(_path);
+//     if (!_route) {
+//       _route = {
+//         path: _path,
+//         name: item.title,
+//         component: Layout,
+//         children: [],
+//       }
+//
+//       _map.set(_path, _route)
+//     }
+//
+//     for (let e of item.children) {
+//       if (!e.path) {
+//         continue
+//       }
+//
+//       let child = {
+//         path: e.path,
+//         name: e.title,
+//         component: e.compo,
+//       };
+//
+//       _route.children.push(child)
+//     }
+//   }
+//
+//   let _routes = []
+//   for (let val of _map.values()) {
+//     _routes.push(val)
+//   }
+//   return _routes;
+// }
+//
+// // add navigator to routes
+// router.addRoutes(_asNavigator(_navigator))
+//
+/* ----------------- custom ----------------- */
